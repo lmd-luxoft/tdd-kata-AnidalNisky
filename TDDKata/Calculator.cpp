825d35c0327f1b75ac54aee8d50e27b4e9627cdc
filonenko@owen.ru <filonenko@owen.ru>
2020-12-01 12:28:04 +0300

    temp

--- TDDKata/Calculator.cpp
+++ TDDKata/Calculator.cpp
@@ -1,12 +1,53 @@
 #include "pch.h"
 #include "Calculator.h"
+#include <stdio.h>
 
 int Calculator::Add(std::string expression)
 {
-    return 0;
+    return -4; //реализации не будет
 }
 
 int Calculator::Add(char* expression)
 {
-    return -4; //Пришлось таки менять, т.к. мы договорились что эксепшенов не поддерживается!
+    num_arg=0;
+    memset(arg_mass, 0x00, sizeof(arg_mass));
+    char cur_symb = 0x00;
+    char* pos = expression;
+    unsigned long number_detected = 0;
+    int res = 0;
+    if (!pos) return 0; //пустая строка возвращает 0
+    if (!*pos) return 0; //пустая строка возвращает 0
+    while (*pos) {
+        if ((expression - pos) > MAX_SIZE_OF_INPUT_STRING) return -1;
+        if (*pos == '-') return -2;
+        if (isdigit(*pos)) {
+            if (number_detected) return -2;
+            number_detected = 1;
+            strncpy_s(arg_mass[num_arg], pos, MAX_SIZE_OF_ARGUMENTS);
+            num_arg++;
+            if (num_arg > MAX_NUM_OF_ARGUMENTS) return -3;
+            pos++;
+            continue;
+        }
+        if (*pos == ',') {
+            if (number_detected) {
+                number_detected = 0;
+                pos++;
+                continue;
+            }
+        }
+        return -2; //неизвестные символы
+    }
+    if (num_arg < MIN_NUM_OF_ARGUMENTS) return -1;
+
+
+    for (long i = 0; i < num_arg; i++) {
+        int arg = 0;
+        if (sscanf_s(arg_mass[i], "%d", &arg) == 1) {
+            res += arg;
+        }else return -2;
+        
+    }
+
+    return res; 
 }
--- TDDKata/test.cpp
+++ TDDKata/test.cpp
@@ -49,3 +49,10 @@ TEST(CalcTest, BAdFormatRetErr) {
 	int actual = calc.Add("5:3 1");
 	ASSERT_EQ(expected, actual);
 }
+
+TEST(CalcTest, FormatWithCRRetOK) {
+	Calculator calc;
+	int expected = 9;
+	int actual = calc.Add("5/n3,1");
+	ASSERT_EQ(expected, actual);
+}
