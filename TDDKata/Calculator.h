09488572a962529d868ae58f93a7155f7b370ac2
filonenko@owen.ru <filonenko@owen.ru>
2020-12-01 10:37:48 +0300

    iteration 1.2

--- TDDKata/Calculator.h
+++ TDDKata/Calculator.h
@@ -1,10 +1,17 @@
 #pragma once
 #include <string>
+#define MAX_NUM_OF_ARGUMENTS	(3)
+#define MIN_NUM_OF_ARGUMENTS	(2)
+#define MAX_SIZE_OF_ARGUMENTS	(1)
+#define MAX_SIZE_OF_INPUT_STRING	(1+1+1+1+1)
 /// <summary>
 /// Class Under test
 /// </summary>
 class Calculator
 {
+	char arg_mass[MAX_NUM_OF_ARGUMENTS][MAX_SIZE_OF_ARGUMENTS+1];
+	int num_arg;
+public:
 	/// <summary>
 	/// Sum of elements expression
 	/// </summary>
