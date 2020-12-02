8951b916b367f0dca076da11fac8e2b382083c14
filonenko@owen.ru <filonenko@owen.ru>
2020-12-02 10:31:44 +0300

    iteration 1.12 Fixtures

---
+++ TDDKata/TextFixture.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Calculator.h"
+
+class TextFixture : public ::testing::Test
+{
+protected:
+	Calculator* calc;
+	void SetUp() override
+	{
+		calc = new Calculator();
+	}
+
+	void TearDown() override
+	{
+		delete calc;
+	}
+};
+
--- TDDKata/test.cpp
+++ TDDKata/test.cpp
@@ -1,147 +1,149 @@
 #include "pch.h"
-#include "Calculator.h"
+#include "TextFixture.h"
 
 
-TEST(CalcTest, NegArgRetManyError1) {
-	Calculator calc;
+TEST_F(TextFixture, NegArgRetManyError1) {
 	int expected = -2;
 	int expected_errors = 2;
-	int actual = calc.Add("-8,-6");
+	int actual = calc->Add("-8,-6");
 	ASSERT_EQ(expected, actual);
+
+
 }
 
-TEST(CalcTest, NegArgRetManyError2) {
-	Calculator calc;
+
+TEST_F(TextFixture, NegArgRetManyError2) {
+	
 	int expected = -2;
 	int expected_errors = 2;
-	int actual = calc.Add("-8,-6");
-	CALCULATOR_ERRORS* perrors = calc.GetLastErrors();
+	int actual = calc->Add("-8,-6");
+	CALCULATOR_ERRORS* perrors = calc->GetLastErrors();
 	ASSERT_NE((void*)perrors, (void*)NULL);
 
 }
-TEST(CalcTest, NegArgRetManyError3) {
-	Calculator calc;
+TEST_F(TextFixture, NegArgRetManyError3) {
+	
 	int expected = -2;
 	int expected_errors = 2;
-	int actual = calc.Add("-8,-6");
-	CALCULATOR_ERRORS* perrors = calc.GetLastErrors();
+	int actual = calc->Add("-8,-6");
+	CALCULATOR_ERRORS* perrors = calc->GetLastErrors();
 	ASSERT_EQ(perrors->num_errors, expected_errors);
 
 }
 
-TEST(CalcTest, NegArgRetManyError4) {
-	Calculator calc;
+TEST_F(TextFixture, NegArgRetManyError4) {
+	
 	int expected = -2;
 	int expected_errors = 2;
-	int actual = calc.Add("-8,-6");
-	CALCULATOR_ERRORS* perrors = calc.GetLastErrors();
+	int actual = calc->Add("-8,-6");
+	CALCULATOR_ERRORS* perrors = calc->GetLastErrors();
 	ASSERT_EQ(perrors->errors[0], expected);
 
 }
 
-TEST(CalcTest, NegArgRetManyError5) {
-	Calculator calc;
+TEST_F(TextFixture, NegArgRetManyError5) {
+	
 	int expected = -2;
 	int expected_errors = 2;
-	int actual = calc.Add("-8,-6");
-	CALCULATOR_ERRORS* perrors = calc.GetLastErrors();
+	int actual = calc->Add("-8,-6");
+	CALCULATOR_ERRORS* perrors = calc->GetLastErrors();
 	ASSERT_EQ(perrors->errors[1], expected);
 
 }
-TEST(CalcTest, OneArgRetError) {
-	Calculator calc;
+TEST_F(TextFixture, OneArgRetError) {
+	
 	int expected = -1;
-	int actual = calc.Add("5");
+	int actual = calc->Add("5");
 	ASSERT_EQ(expected, actual);
 }
-TEST(CalcTest, FormatWithDelimRetOK) {
-	Calculator calc;
+TEST_F(TextFixture, FormatWithDelimRetOK) {
+	
 	int expected = 9;
-	int actual = calc.Add("//:\n5\n3,1");
+	int actual = calc->Add("//:\n5\n3,1");
 	ASSERT_EQ(expected, actual);
 }
 
-TEST(CalcTest, FormatWithBadDelimRetBad) {
-	Calculator calc;
+TEST_F(TextFixture, FormatWithBadDelimRetBad) {
+	
 	int expected = -2;
-	int actual = calc.Add("/:\n5\n3,1");
+	int actual = calc->Add("/:\n5\n3,1");
 	ASSERT_EQ(expected, actual);
 }
 
-TEST(CalcTest, FormatWithBad2DelimRetBad) {
-	Calculator calc;
+TEST_F(TextFixture, FormatWithBad2DelimRetBad) {
+	
 	int expected = -4;
-	int actual = calc.Add("//7\n5\n3,1");
+	int actual = calc->Add("//7\n5\n3,1");
 	ASSERT_EQ(expected, actual);
 }
-TEST(CalcTest, EmptyStringEQZero) { 
-	Calculator calc; 
+TEST_F(TextFixture, EmptyStringEQZero) { 
+	 
 	int expected = 0; 
-	int actual = calc.Add(""); 
+	int actual = calc->Add(""); 
 	ASSERT_EQ(expected, actual); 
 }
 
-TEST(CalcTest, BigArgRetError) {
-	Calculator calc;
+TEST_F(TextFixture, BigArgRetError) {
+	
 	int expected = -2;
-	int actual = calc.Add("55,5");
+	int actual = calc->Add("55,5");
 	ASSERT_EQ(expected, actual);
 }
 
-TEST(CalcTest, TwoArgRetOK) {
-	Calculator calc;
+TEST_F(TextFixture, TwoArgRetOK) {
+	
 	int expected = 8;
-	int actual = calc.Add("5,3"); 
+	int actual = calc->Add("5,3"); 
 	ASSERT_EQ(expected, actual);
 }
 
-TEST(CalcTest, ThreeArgRetOK) {
-	Calculator calc;
+TEST_F(TextFixture, ThreeArgRetOK) {
+	
 	int expected = 9;
-	int actual = calc.Add("5,3,1");
+	int actual = calc->Add("5,3,1");
 	ASSERT_EQ(expected, actual); 
 }
 
-TEST(CalcTest, FourArgRetOK) {
-	Calculator calc;
+TEST_F(TextFixture, FourArgRetOK) {
+	
 	int expected = 17;
-	int actual = calc.Add("5,3,1,8");
+	int actual = calc->Add("5,3,1,8");
 	ASSERT_EQ(expected, actual);
 }
 
 
-TEST(CalcTest, NegArgRetErr) {
-	Calculator calc;
+TEST_F(TextFixture, NegArgRetErr) {
+	
 	int expected = -2;
-	int actual = calc.Add("-5,5");
+	int actual = calc->Add("-5,5");
 	ASSERT_EQ(expected, actual);
 }
 
-TEST(CalcTest, BAdFormatRetErr) {
-	Calculator calc;
+TEST_F(TextFixture, BAdFormatRetErr) {
+	
 	int expected = -2;
-	int actual = calc.Add("5:3 1");
+	int actual = calc->Add("5:3 1");
 	ASSERT_EQ(expected, actual);
 }
 
-TEST(CalcTest, BigFormatRetErr) {
-	Calculator calc;
+TEST_F(TextFixture, BigFormatRetErr) {
+	
 	int expected = -3;
-	int actual = calc.Add("1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1");
+	int actual = calc->Add("1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1");
 	ASSERT_EQ(expected, actual);
 }
 
-TEST(CalcTest, NotVeryBigFormatRetOK) {
-	Calculator calc;
+TEST_F(TextFixture, NotVeryBigFormatRetOK) {
+	
 	int expected = 16;
-	int actual = calc.Add("1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1");
+	int actual = calc->Add("1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1");
 	ASSERT_EQ(expected, actual);
 }
 
-TEST(CalcTest, FormatWithCRRetOK) {
-	Calculator calc;
+TEST_F(TextFixture, FormatWithCRRetOK) {
+	
 	int expected = 9;
-	int actual = calc.Add("5\n3,1");
+	int actual = calc->Add("5\n3,1");
 	ASSERT_EQ(expected, actual);
 }
 
